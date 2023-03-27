c0e891fc964a58e339ade6d82525d40426d14825
keattipong4460@gmail.com <tonskz@TonsKz.local>
2023-03-27 15:38:38 +0700

    first commit

---
+++ 1.js
@@ -0,0 +1,10 @@
+let  {Names , Age ,Address, tel} = {
+    Names: "Keattipong",
+    Age: 27,
+    Address: "123/4 BKK",
+    tel: 98082345
+}
+
+let person = `ชื่อ : ${Names}`
+
+console.log(person)
